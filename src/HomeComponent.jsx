b3b02a378149eb63a3bb79e401ce7dde92ae40ac
Sean Narron <seannarron9@gmail.com>
2021-09-09 11:47:54 +0900

    Created a Home Component

---
+++ src/HomeComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ItemsComponent from "./ItemsComponent";
+
+function HomeComponent() {
+
+    
+    return (
+        <div>
+            <h1>Hello, Welcome to Todo</h1>;
+            <ItemsComponent />
+
+        </div>
+    )
+            
+
+      
+}
+
+export default HomeComponent
