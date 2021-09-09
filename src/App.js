9948fa40e6301ec966ee966eb60cd825d0addba0
Sean Narron <seannarron9@gmail.com>
2021-09-09 11:50:57 +0900

    Moved files to components folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import HomeComponent from './HomeComponent';
+import HomeComponent from './Components/HomeComponent';
 
 function App() {
   return (
