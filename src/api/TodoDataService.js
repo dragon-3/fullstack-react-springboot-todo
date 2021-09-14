9fd23df06aabac001ae1ebc08aa7bd49ae3e0d88
Sean Narron <seannarron9@gmail.com>
2021-09-14 15:02:34 +0900

    Added api for delete

--- src/api/TodoDataService.js
+++ src/api/TodoDataService.js
@@ -5,8 +5,13 @@ class TodoDataService {
     retriveAllTodos() {
         return axios.get("http://localhost:8082/all-todos");
     }
+
     retriveTodo(id) {
         return axios.get(`http://localhost:8082/all-todos/${id}`);
+    }
+
+    deleteTodo(id) {
+        return axios.delete(`http://localhost:8082/all-todos/${id}`);
     }
 }
 
