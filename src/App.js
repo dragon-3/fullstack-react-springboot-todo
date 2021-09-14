b988e78b2c572cf00f1243ccdd2fec142f5cea91
Sean Narron <seannarron9@gmail.com>
2021-09-14 15:02:22 +0900

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeComponent from './components/HomeComponent';
+import ItemComponent from './components/ItemComponent';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <HomeComponent />
+      
+      <Router>
+        <>
+          <Switch>
+            <Route path="/all-todos/:id" exact component={ItemComponent} />
+            <HomeComponent />
+          </Switch>
+        </>
+    </Router>
     </div>
   );
 }
